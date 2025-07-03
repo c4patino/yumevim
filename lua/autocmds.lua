79991c4246fd7998de7a5c203b2f1101c3616ead
c4patino <79673111+c4patino@users.noreply.github.com>
2025-07-03 00:05:30 +0000

    style: autoformatting all lua files
    [skip ci]

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,32 +1,32 @@
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
-    end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
+	end,
 })
 
 -- Set 2-space tabs for specific filetypes
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "astro", "javascript", "javascriptreact", "nix", "tex", "typescript", "typescriptreact" },
-    callback = function()
-        vim.opt_local.tabstop = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.expandtab = true
-    end,
+	pattern = { "astro", "javascript", "javascriptreact", "nix", "tex", "typescript", "typescriptreact" },
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end,
 })
 
 -- Enable wrap and ZenMode toggle for LaTeX files
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "tex",
-    callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.linebreak = true
-        vim.keymap.set("n", "<leader>zz", function()
-            if require("zen-mode.view").is_open() then
-                require("zen-mode").toggle()
-            else
-                require("zen-mode").toggle({ window = { width = 85 } })
-            end
-        end, { buffer = true, desc = "ZenMode toggle" })
-    end,
+	pattern = "tex",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.keymap.set("n", "<leader>zz", function()
+			if require("zen-mode.view").is_open() then
+				require("zen-mode").toggle()
+			else
+				require("zen-mode").toggle({ window = { width = 85 } })
+			end
+		end, { buffer = true, desc = "ZenMode toggle" })
+	end,
 })
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,14 +9,18 @@ keymap("n", "<C-d>", "<C-d>zz", { desc = "Vertical Scroll down" })
 
 keymap("n", "n", "nzzzv", { desc = "Find/Replace Move to next match" })
 keymap("n", "N", "Nzzzv", { desc = "Find/Replace Move to previous match" })
-keymap("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
-    { desc = "Find/Replace Replace current word" })
+keymap(
+	"n",
+	"<leader>s",
+	":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+	{ desc = "Find/Replace Replace current word" }
+)
 
 keymap("v", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Editing Move line down" })
 keymap("v", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Editing Move line up" })
 
-keymap({ "n", "v" }, "<leader>y", "\"+y", { desc = "Editing copy to system clipboard" })
-keymap("v", "<leader>Y", "\"+Y", { desc = "Editing Copy line to system clipboard" })
+keymap({ "n", "v" }, "<leader>y", '"+y', { desc = "Editing copy to system clipboard" })
+keymap("v", "<leader>Y", '"+Y', { desc = "Editing Copy line to system clipboard" })
 
-keymap("v", "<leader>p", "\"_dP", { desc = "Editing Replace text with paste buffer" })
-keymap({ "n", "v" }, "<leader>d", "\"_d", { desc = "Editing Delete without overwriting register" })
+keymap("v", "<leader>p", '"_dP', { desc = "Editing Replace text with paste buffer" })
+keymap({ "n", "v" }, "<leader>d", '"_d', { desc = "Editing Delete without overwriting register" })
