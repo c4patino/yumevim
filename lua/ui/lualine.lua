812bf4041b88a7f0a8ad9c506f038d9a04117701
C4 Patino <c4patino@gmail.com>
2025-07-05 13:18:49 -0500

    feat: added recording to lualine output

--- lua/ui/lualine.lua
+++ lua/ui/lualine.lua
@@ -1,36 +1,45 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	event = "VeryLazy",
-	config = function()
-		require("lualine").setup({
-			options = {
-				globalstatus = true,
-				always_divide_middle = true,
-				theme = "auto",
-				ignore_focus = { "nvim-tree" },
-				component_separators = {
-					left = "|",
-					right = "|",
-				},
-			},
-			extensions = { "fzf" },
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = {
-					{ "branch", icon = "", icons_enabled = true },
-					{ "diff" },
-					{ "diagnostics" },
-				},
-				lualine_c = { "filename" },
-				lualine_x = { "filetype" },
-				lualine_y = { "location" },
-				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
-				},
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    config = function()
+        require("lualine").setup({
+            options = {
+                globalstatus = true,
+                always_divide_middle = true,
+                theme = "auto",
+                ignore_focus = { "nvim-tree" },
+                component_separators = {
+                    left = "|",
+                    right = "|",
+                },
+            },
+            extensions = { "fzf" },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = {
+                    { "branch", icon = "", icons_enabled = true },
+                    { "diff" },
+                    { "diagnostics" },
+                },
+                lualine_c = {
+                    "filename",
+                    function()
+                        local rec = vim.fn.reg_recording()
+                        if rec ~= "" then
+                            return " @" .. rec
+                        end
+                        return ""
+                    end,
+                },
+                lualine_x = { "filetype" },
+                lualine_y = { "location" },
+                lualine_z = {
+                    function()
+                        return " " .. os.date("%R")
+                    end,
+                },
+            },
+        })
+    end,
 }
