dcd1cdbbbc23b0428641b940900cd40e3a4eea15
c4patino <79673111+c4patino@users.noreply.github.com>
2025-08-31 21:49:46 +0000

    style: autoformatting all lua files
    [skip ci]

--- lua/ui/noice.lua
+++ lua/ui/noice.lua
@@ -1,48 +1,48 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-    },
-    opts = {
-        notify = {
-            enabled = true,
-        },
-        messages = {
-            enabled = true,
-        },
-        lsp = {
-            message = {
-                enabled = true,
-            },
-            progress = {
-                enabled = false,
-                view = "mini",
-            },
-        },
-        popupmenu = {
-            enabled = true,
-            backend = "nui",
-        },
-        format = {
-            filter = {
-                pattern = { ":%s*%%s*s:%s*", ":%s*%%s*s!%s*", ":%s*%%s*s/%s*", "%s*s:%s*", ":%s*s!%s*", ":%s*s/%s*" },
-                icon = "",
-                lang = "regex",
-            },
-            replace = {
-                pattern = {
-                    ":%s*%%s*s:%w*:%s*",
-                    ":%s*%%s*s!%w*!%s*",
-                    ":%s*%%s*s/%w*/%s*",
-                    "%s*s:%w*:%s*",
-                    ":%s*s!%w*!%s*",
-                    ":%s*s/%w*/%s*",
-                },
-                icon = "󱞪",
-                lang = "regex",
-            },
-        },
-    },
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		notify = {
+			enabled = true,
+		},
+		messages = {
+			enabled = true,
+		},
+		lsp = {
+			message = {
+				enabled = true,
+			},
+			progress = {
+				enabled = false,
+				view = "mini",
+			},
+		},
+		popupmenu = {
+			enabled = true,
+			backend = "nui",
+		},
+		format = {
+			filter = {
+				pattern = { ":%s*%%s*s:%s*", ":%s*%%s*s!%s*", ":%s*%%s*s/%s*", "%s*s:%s*", ":%s*s!%s*", ":%s*s/%s*" },
+				icon = "",
+				lang = "regex",
+			},
+			replace = {
+				pattern = {
+					":%s*%%s*s:%w*:%s*",
+					":%s*%%s*s!%w*!%s*",
+					":%s*%%s*s/%w*/%s*",
+					"%s*s:%w*:%s*",
+					":%s*s!%w*!%s*",
+					":%s*s/%w*/%s*",
+				},
+				icon = "󱞪",
+				lang = "regex",
+			},
+		},
+	},
 }
