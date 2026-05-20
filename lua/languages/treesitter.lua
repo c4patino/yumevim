54821b57eb32c0fe064938ca4baca94bf49cb117
C4 Patino <c4patino@gmail.com>
2026-05-20 13:41:37 -0500

    feat: update nvim-treesitter to no longer require nvim-treesitter.configs

--- lua/languages/treesitter.lua
+++ lua/languages/treesitter.lua
@@ -2,12 +2,6 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-			},
-		})
-
 		vim.opt.foldmethod = "expr"
 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 	end,
